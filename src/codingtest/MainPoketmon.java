67331f57fa01f648d297ecf460c93ee93180d6f5
who(true) <alexkjh2011@gmail.com>
2022-11-05 01:28:49 +0900

    put parameter in Poketmon and print

--- src/codingtest/MainPoketmon.java
+++ src/codingtest/MainPoketmon.java
@@ -3,3 +3,11 @@ package codingtest;
 public class MainPoketmon {
     public static void main(String[] args) {
         Poketmon poketmon = new Poketmon();
+
+        int arr[] = new int[]{3,3,3,2,2,4};
+        poketmon.solution(arr);
+        System.out.println(poketmon.solution(arr));
+
+
+    }
+}
