772f3756807db747aebc00e6ffaa61268186bb75
who(true) <alexkjh2011@gmail.com>
2022-11-05 01:29:22 +0900

    make a new parameter of outcall

--- src/codingtest/MainIHateSameNum.java
+++ src/codingtest/MainIHateSameNum.java
@@ -3,8 +3,8 @@ package codingtest;
 public class MainIHateSameNum {
     public static void main(String[] args) {
         IHateSameNum iHateSameNum = new IHateSameNum();
-       int[] arr = new int[]{1,1,0,3,3,4,4}; //
-        iHateSameNum.solution(arr);
+       int[] arr = new int[]{1,1,0,3,3,4,4};
+        int[] arr2 = new int[]{1,0,1,3,4,4};
         System.out.println(iHateSameNum.solution(arr));
 
     }
