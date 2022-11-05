9a3a0d8bb28cf5751d67d5a063a7b4fb32fa70bd
who(true) <alexkjh2011@gmail.com>
2022-11-06 01:34:28 +0900

    make a solution method in Poketmon class

--- src/codingtest/Poketmon.java
+++ src/codingtest/Poketmon.java
@@ -3,3 +3,7 @@ package codingtest;
 import java.util.*;
 
 public class Poketmon { // n 포켓몬의 수중 나누기 2만큼, 하지만 똑같은 종류의 포켓몬은 제외가장 많은 종류의 포켓몬을 선택하는 방법을 찾기, 갯수를 리턴
+    public int solution(int[] nums) {
+//        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+
