29c835262541afe58987790a7a24e5ce0dbd12ba
who(true) <alexkjh2011@gmail.com>
2022-11-07 00:54:54 +0900

    add new parameter

--- src/codingtest/MainIHateSameNum.java
+++ src/codingtest/MainIHateSameNum.java
@@ -4,7 +4,7 @@ public class MainIHateSameNum {
     public static void main(String[] args) {
         IHateSameNum iHateSameNum = new IHateSameNum();
        int[] arr = new int[]{1,1,0,3,3,4,4};
-        int[] arr2 = new int[]{1,0,1,3,4,4};
+         int[] arr2 = new int[]{1,0,1,3,4,4};
         System.out.println(iHateSameNum.solution(arr));
 
     }
