371b2752ec6680d82f827918255b1d3f790cb080
who(true) <alexkjh2011@gmail.com>
2022-11-07 00:58:38 +0900

    call & put a parameter value in MainStock

--- src/codingtest/MainStockPrice.java
+++ src/codingtest/MainStockPrice.java
@@ -5,3 +5,7 @@ import java.util.Arrays;
 public class MainStockPrice {
     public static void main(String[] args) {
         StockPrice stockPrice = new StockPrice();
+        int[] a = new int[]{1,2,3,2,3};
+        System.out.println(Arrays.toString(stockPrice.sol(a)));
+    }
+}
