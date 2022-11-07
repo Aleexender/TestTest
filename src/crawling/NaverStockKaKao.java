1bdcd192aae036068f3bf3b63947a1d0512f06b9
who(true) <alexkjh2011@gmail.com>
2022-11-08 01:22:01 +0900

    Create NaverStockKaKao.java

---
+++ src/crawling/NaverStockKaKao.java
@@ -0,0 +1,10 @@
+package crawling;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class NaverStockKaKao {
+    public static void main(String[] args) {
