706f9708217bdc35fafa90d39f1109113cf4431d
who(true) <alexkjh2011@gmail.com>
2022-11-08 01:29:11 +0900

    use try method and put instance variables and print

--- src/crawling/NaverStockKaKao.java
+++ src/crawling/NaverStockKaKao.java
@@ -11,3 +11,23 @@ public class NaverStockKaKao {
 
         String URL = "https://finance.naver.com/item/main.naver?code=035720";
         Document doc;
+
+        try {
+            doc = Jsoup.connect(URL).get();
+            Elements element = doc.select(".date");
+
+            Elements totalPrice = doc.getElementsByAttributeValue("class","no_today").get(0).getElementsByAttributeValue("class","blind");
+            String price = totalPrice.text();
+
+            Elements highestPrice = doc.getElementsByAttributeValue("class", "no_up").get(0).getElementsByAttributeValue("class","blind");
+            String highPrice = highestPrice.text();
+
+            Elements lowestPrice = doc.getElementsByAttributeValue("class", "no_down").get(0).getElementsByAttributeValue("class","blind");
+            String lowPrice = lowestPrice.text();
+
+
+            System.out.println(price);
+            System.out.println(highPrice);
+            System.out.println(lowPrice);
+
+
