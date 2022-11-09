114220ed283cca23a7a67a0323f4bb86116d447d
who(true) <alexkjh2011@gmail.com>
2022-11-09 20:15:35 +0900

    make a for() in StockPrice class

--- src/codingtest/StockPrice.java
+++ src/codingtest/StockPrice.java
@@ -3,3 +3,8 @@ package codingtest;
 public class StockPrice {
     public int[] sol(int[] prices) {
         int[] answer = new int[prices.length]; // pirce.length 때문에 array.toString()필요
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = i + 1; j < answer.length; j++) {
+                answer[i]++;// 리스트 값 추가
+
