7c100b2717a9d8195b778111bfc65ce0ec3b7939
who(true) <alexkjh2011@gmail.com>
2022-11-10 19:02:29 +0900

    put a Scanner instance in PoanoLevel

--- src/codingtest/PianoLevel.java
+++ src/codingtest/PianoLevel.java
@@ -5,3 +5,5 @@ import java.util.Scanner;
 
 public class PianoLevel {
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in); //입력값을 바이트 단위로 읽는것
+
