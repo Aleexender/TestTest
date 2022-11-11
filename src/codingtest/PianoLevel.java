f59b79c4845df9407d62ed8630bc79cdc0f799a9
who(true) <alexkjh2011@gmail.com>
2022-11-11 23:23:40 +0900

    Update PianoLevel.java

--- src/codingtest/PianoLevel.java
+++ src/codingtest/PianoLevel.java
@@ -7,3 +7,7 @@ public class PianoLevel {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in); //입력값을 바이트 단위로 읽는것
 
+        int[] data = new int[]{1,2,3,4,5,6,7,8};
+        boolean ascending = true;
+        boolean descending = true;
+
