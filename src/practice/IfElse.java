09e077138ac0ebeef716436f261df5ce5aad1512
Alex Kim <alexkjh2011@gmail.com>
2022-11-14 00:42:17 +0900

    use ternary operator

--- src/practice/IfElse.java
+++ src/practice/IfElse.java
@@ -4,3 +4,6 @@ public class IfElse {
     public void solution() {
         int ben = 10;
         int kevin = 15;
+
+        String answer = (ben < kevin) ? "kevin이 형이라고!" : "kevin이 형이 아니라고!";
+        System.out.println(answer);
