5f76425e4bbe6f77fa45b990a00f3c6fb4f20440
Alex Kim <alexkjh2011@gmail.com>
2022-11-16 02:35:29 +0900

    add information in PianoLevel

--- src/codingtest/PianoLevel.java
+++ src/codingtest/PianoLevel.java
@@ -7,10 +7,12 @@ public class PianoLevel {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in); //입력값을 바이트 단위로 읽는것
 
-        int[] data = new int[]{1,2,3,4,5,6,7,8};
+        int[] data = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
         boolean ascending = true;
         boolean descending = true;
 
-        for (int i = 0; i < 8; i++ ) {
+        for (int i = 0; i < 8; i++) {
             data[i] = scanner.nextInt(); // int를 정수로 받는 메소드
         }
+    }
+}
