ca77ecb2b35bc5683312233a1de34c566859a001
Alex Kim <alexkjh2011@gmail.com>
2022-11-19 01:15:09 +0900

    make constructor in PrThread and put for()

--- src/practice/PrThread.java
+++ src/practice/PrThread.java
@@ -2,3 +2,11 @@ package practice;
 
 public class PrThread implements Runnable {
     private int[] temp;
+
+    public PrThread() {
+        temp = new int[10];
+
+        for (int start = 0; start < temp.length; start++) {
+            temp[start] = start;
+        }
+    }
