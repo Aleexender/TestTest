208989cf7b7deb66668e335591577c74f17f169c
Alex Kim <alexkjh2011@gmail.com>
2022-11-19 01:15:45 +0900

    make run method and put try and catch in for

--- src/practice/PrThread.java
+++ src/practice/PrThread.java
@@ -10,3 +10,16 @@ public class PrThread implements Runnable {
             temp[start] = start;
         }
     }
+    @Override
+    public void run() {
+        for (int start2 : temp) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(start2);
+        }
+    }
+}
