6333c4e11c11bba1f6701a1c80f507fab19b9ce0
Alex Kim <alexkjh2011@gmail.com>
2022-11-20 23:44:52 +0900

    call a PrThread instance

--- src/practice/ThreadMain.java
+++ src/practice/ThreadMain.java
@@ -2,3 +2,10 @@ package practice;
 
 public class ThreadMain {
     public static void main(String[] args) {
+
+        PrThread th = new PrThread();
+        Thread t = new Thread(th, "first");
+
+        t.start();
+    }
+}
