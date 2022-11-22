0470dd0bb77941910a01b9ac889776dc391b4e89
Alex Kim <alexkjh2011@gmail.com>
2022-11-23 03:04:55 +0900

    use synchronized be thread safe(double checking)

--- src/practice/Singleton.java
+++ src/practice/Singleton.java
@@ -5,3 +5,11 @@ import java.io.Serializable;
 public class Singleton implements Serializable {
     private static volatile Singleton instance; // volatile
 
+    public static Singleton getInstance() { // 이 위치에 synchronized 에 있는거 보다
+        if (instance == null) {
+            synchronized (Singleton.class) { // 이 위치가 synchronized 더 효율적
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
