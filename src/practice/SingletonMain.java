4de6c82f1e06934a95a317e38200f41fafa46bca
Alex Kim <alexkjh2011@gmail.com>
2022-11-26 13:23:27 +0900

    make a constructor( reflection) in SingletonMain class

--- src/practice/SingletonMain.java
+++ src/practice/SingletonMain.java
@@ -9,3 +9,5 @@ public class SingletonMain {
         Singleton singleton = Singleton.getInstance();
         Singleton singleton1 = Singleton.getInstance();
         Singleton singleton3 = null;
+
+        Constructor<Singleton> constructor = Singleton.class.getDeclaredConstructor(); // 리플렉션 (싱글톤을 깨트리는 방법)
