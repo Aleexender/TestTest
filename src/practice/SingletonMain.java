6afde906dc72fd53fa8265c39ed2eeacfefd2030
Alex Kim <alexkjh2011@gmail.com>
2022-11-26 13:22:47 +0900

    make a instance in SingletonMain

--- src/practice/SingletonMain.java
+++ src/practice/SingletonMain.java
@@ -6,3 +6,6 @@ import java.lang.reflect.InvocationTargetException;
 
 public class SingletonMain {
     public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, IOException, ClassNotFoundException {
+        Singleton singleton = Singleton.getInstance();
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton3 = null;
