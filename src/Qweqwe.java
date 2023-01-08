4f64acbd74978953ce91256b9eadd191c6dc8c95
Alex Kim <alexkjh2011@gmail.com>
2023-01-09 03:19:46 +0900

    make instance variables and try to debugging Query in console

--- src/Qweqwe.java
+++ src/Qweqwe.java
@@ -1,2 +1,17 @@
 public class Qweqwe {
     public static void main(String[] args) {
+            String a = "샤넬";
+//            System.out.println("VALUES('"+a+"')");
+        String age = "27";
+        String job = "무직";
+        String id = "404";
+
+//        System.out.println("김정훈("+age+",'"+job+"')");
+//        System.out.println("김정훈("+age+", +1 )");
+//        System.out.println(("INSERT INTO member(id,count)\n" + "VALUES( '" + "id" + "', +1)"));
+
+        System.out.println("INSERT INTO member (`id` , `count`)\n" + "VALUES ('"+id+"', 1 ) ON DUPLICATE KEY UPDATE `count`=  `count`+1;");
+        System.out.println();
+        System.out.println("INSERT INTO member (`id` , `count`)\n" + "VALUES (id, 1 ) ON DUPLICATE KEY UPDATE `count`=  `count`+1;");
+    }
+    }
