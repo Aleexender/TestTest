befcafd3a99dc34e320543d8b348c1a61ee9fbcb
Alex Kim <alexkjh2011@gmail.com>
2023-01-12 01:40:34 +0900

    put a for() and add it to list

--- src/Average.java
+++ src/Average.java
@@ -4,3 +4,6 @@ import java.util.List;
 public class Average {
     public double solution(int[] numbers) {
         List<Integer> qwe2 = new ArrayList<>();
+        for (int qwe : numbers) {
+            qwe2.add(qwe);
+        }
