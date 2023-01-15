98ce2e7cfbe6ff53d631a36ff390b0b7b7875858
Alex Kim <alexkjh2011@gmail.com>
2023-01-15 23:22:08 +0900

    didn't launch the Jsoup file

--- src/crawling/NaverStockKaKao.java
+++ src/crawling/NaverStockKaKao.java
@@ -1,38 +1,38 @@
-package crawling;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-
-public class   NaverStockKaKao {
-    public static void main(String[] args) {
-
-        String URL = "https://finance.naver.com/item/main.naver?code=035720";
-        Document doc;
-
-        try {
-            doc = Jsoup.connect(URL).get();
-            Elements element = doc.select(".date");
-
-            Elements totalPrice = doc.getElementsByAttributeValue("class","no_today").get(0).getElementsByAttributeValue("class","blind");
-            String price = totalPrice.text();
-
-            Elements highestPrice = doc.getElementsByAttributeValue("class", "no_up").get(0).getElementsByAttributeValue("class","blind");
-            String highPrice = highestPrice.text();
-
-            Elements lowestPrice = doc.getElementsByAttributeValue("class", "no_down").get(3).getElementsByAttributeValue("class","blind");
-            String lowPrice = lowestPrice.text();
-
-
-            System.out.println(price);
-            System.out.println(highPrice);
-            System.out.println(lowPrice);
-
-
-        } catch (IOException e) {
-            System.out.println("오류임");
-        }
-    }
-}
+//package crawling;
+//
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.select.Elements;
+//
+//import java.io.IOException;
+//
+//public class   NaverStockKaKao {
+//    public static void main(String[] args) {
+//
+//        String URL = "https://finance.naver.com/item/main.naver?code=035720";
+//        Document doc;
+//
+//        try {
+//            doc = Jsoup.connect(URL).get();
+//            Elements element = doc.select(".date");
+//
+//            Elements totalPrice = doc.getElementsByAttributeValue("class","no_today").get(0).getElementsByAttributeValue("class","blind");
+//            String price = totalPrice.text();
+//
+//            Elements highestPrice = doc.getElementsByAttributeValue("class", "no_up").get(0).getElementsByAttributeValue("class","blind");
+//            String highPrice = highestPrice.text();
+//
+//            Elements lowestPrice = doc.getElementsByAttributeValue("class", "no_down").get(0).getElementsByAttributeValue("class","blind");
+//            String lowPrice = lowestPrice.text();
+//
+//
+//            System.out.println(price);
+//            System.out.println(highPrice);
+//            System.out.println(lowPrice);
+//
+//
+//        } catch (IOException e) {
+//            System.out.println("오류임");
+//        }
+//    }
+//}
