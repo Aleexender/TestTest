8c7092fab9419e4c0f90ef65ff1cdc78e877b874
Alex Kim <alexkjh2011@gmail.com>
2023-01-31 05:06:17 +0900

    make a student profile method

--- src/inheritance/ExtendCH.java
+++ src/inheritance/ExtendCH.java
@@ -1,3 +1,12 @@
 package inheritance;
 
 public class ExtendCH extends ExtendPa {
+    ExtendPa parent = new ExtendPa();
+
+
+    public void studentProfile() {
+        parent.setName("정훈");
+        parent.setAge(25);
+        System.out.println(parent.getAge());
+        System.out.println(parent.getName());
+    }
