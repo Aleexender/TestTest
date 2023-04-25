ac36f73b64ec04159465721b90c0322cd6923a7a
Alex Kim <alexkjh2011@gmail.com>
2023-04-25 22:31:32 +0200

    print it

--- src/OOP/House.java
+++ src/OOP/House.java
@@ -5,3 +5,8 @@ public class House {
         Elice elice = new Elice();
         Juice juice = new Juice();
 
+
+        System.out.println("엘리스가 음료를 마신후 음료의 용량은= "+elice.drink() +"ml");
+        System.out.println("주스를 마심으로써 엘리스의 키는= "+ juice.ability() + "cm");
+    }
+}
