50cf2fe8538619821965cd87c2636a2f51180d86
Alex Kim <alexkjh2011@gmail.com>
2023-05-29 18:07:58 +0200

    Create LamdaPractice.java

---
+++ src/lamda/LamdaPractice.java
@@ -0,0 +1,13 @@
+package lamda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LamdaPractice {
+
+    public int math(int a, int b) {
+        return a + b;
+    }
+
