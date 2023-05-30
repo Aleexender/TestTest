72553e9d09b4336a4e92e6bd2e384ae784398165
Alex Kim <alexkjh2011@gmail.com>
2023-05-30 23:42:44 +0200

    make hash and instance variables

--- src/HashTest.java
+++ src/HashTest.java
@@ -5,3 +5,8 @@ import java.util.List;
 
 public class HashTest {
     public static void main(String[] args) {
+        String[] report = new String[100];
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        String[] victim = new String[report.length];
+        String[] reporter = new String[report.length];
+
