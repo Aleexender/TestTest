7f6f32adedb029ce53b5070530daa43f2c6b1702
Alex Kim <alexkjh2011@gmail.com>
2023-06-01 19:04:27 +0200

    make for() or print it

--- src/CutByIndexArr.java
+++ src/CutByIndexArr.java
@@ -3,5 +3,27 @@ import java.util.Arrays;
 public class CutByIndexArr {
     public static void main(String[] args) {
         int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = i;
+        }
         int[] arr2 = Arrays.copyOfRange(arr, 3, 7);
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr2));
+
+
         int[][] arr3 = new int[3][3];
+
+        for(int i = 0; i <arr3.length; i++) {
+            int add = 0;
+            for(int k = 0; k < arr3[i].length; k++){
+
+                add += 10;
+                arr3[i][k] = add;
+
+                System.out.print(arr3[i][k]);
+            }
+            System.out.println();
+        }
+    }
+}
