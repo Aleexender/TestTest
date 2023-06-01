c0303e030697b834e7f8fbb8ef96731c71b1ec5a
Alex Kim <alexkjh2011@gmail.com>
2023-06-01 19:04:15 +0200

    make instance variables

--- src/CutByIndexArr.java
+++ src/CutByIndexArr.java
@@ -2,3 +2,6 @@ import java.util.Arrays;
 
 public class CutByIndexArr {
     public static void main(String[] args) {
+        int[] arr = new int[10];
+        int[] arr2 = Arrays.copyOfRange(arr, 3, 7);
+        int[][] arr3 = new int[3][3];
