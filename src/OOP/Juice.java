03fab18bea7b39862d3aa9cbbed3fd04620cc9e2
Alex Kim <alexkjh2011@gmail.com>
2023-06-06 01:02:09 +0200

    Create Juice.java

---
+++ src/OOP/Juice.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class Juice extends Elice {
+    int ml = 100;
+
+
+    public int ability() {
+        Elice elice = new Elice();
+        return super.height -= 40;
+    }
+}
