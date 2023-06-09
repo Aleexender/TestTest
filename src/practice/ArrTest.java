430e4434f4753c5edf181677d31e32d20b2b4572
Alex Kim <alexkjh2011@gmail.com>
2023-06-09 18:41:49 +0200

    Create ArrTest.java

---
+++ src/practice/ArrTest.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class ArrTest {
+    public static void main(String[] args) {
+        String[] park = {"SOO","OOO","OOO","OOO"};
+        System.out.println(park.length);
+        System.out.println(park[0].length());
+    }
+}
