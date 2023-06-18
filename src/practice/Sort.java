bc78fff39811348b9c79190d2cffa7fffe2d789b
Alex Kim <alexkjh2011@gmail.com>
2023-06-19 05:09:16 +0900

    Practice sorting

---
+++ src/practice/Sort.java
@@ -0,0 +1,15 @@
+package practice;
+
+import java.util.Arrays;
+
+public class Sort {
+    public static void main(String[] args) {
+        int[] qwe = {1, 9, 5, 6, 11, 10};
+
+        Arrays.sort(qwe);
+
+        for (int q:qwe) {
+            System.out.println(q);
+        }
+    }
+}
