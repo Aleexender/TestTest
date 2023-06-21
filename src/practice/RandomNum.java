43da812db5a3eec762408836b96ac3329d8b3f5b
Alex Kim <alexkjh2011@gmail.com>
2023-06-22 08:03:24 +0900

    practice

---
+++ src/practice/RandomNum.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class RandomNum {
+    public static void main(String[] args) {
+        int a = (int) (Math.random() * 999);
+
+        System.out.println(a);
+
+        int b = 123;
+
+        System.out.println(123 / 10);
+    }
+
+}
