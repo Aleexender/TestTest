cc7d9ff0297bee2411be403ec14da5572719a3ae
Aleexender <alexkjh2011@gmail.com>
2023-12-15 10:03:21 +0900

    Update ExtendMain.java

--- src/inheritance/ExtendMain.java
+++ src/inheritance/ExtendMain.java
@@ -2,3 +2,9 @@ package inheritance;
 
 public class ExtendMain {
     public static void main(String[] args) {
+        ExtendCH child = new ExtendCH();
+        child.studentProfile();
+        child.parentSkill();
+
+    }
+}
