78de82702c49b022cb7dae092c8073309ec21b7c
Aleexender <alexkjh2011@gmail.com>
2023-12-25 04:10:45 +0900

    Update ArrayListLinkedList.java

--- src/practice/ArrayListLinkedList.java
+++ src/practice/ArrayListLinkedList.java
@@ -8,14 +8,15 @@ public class ArrayListLinkedList {
 
         integerArrayList.add(1);
         integerArrayList.add(2);
-        integerArrayList.add(0,3);
+        integerArrayList.add(0, 3);
 
-        integerArrayList.set(0,4);
+        integerArrayList.set(0, 4);
         integerArrayList.remove(0);
         integerArrayList.get(1);
 
         System.out.println(integerArrayList);
 //        System.out.println(integerArrayList.get(1));
 
+        System.out.println();
     }
 }
