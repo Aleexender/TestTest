a3b3fdcd449ed0bf8eba2c0b9b6967c359873cb5
Aleexender <alexkjh2011@gmail.com>
2023-12-31 17:41:40 +0900

    Update ArrayListTest.java

--- src/practice/ArrayListTest.java
+++ src/practice/ArrayListTest.java
@@ -1,2 +1,19 @@
-package practice;public class ArrayListTest {
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(null);
+        list.add(1);
+        list.add(1);
+
+        for (int i :list) {
+
+        }
+
+    }
 }
