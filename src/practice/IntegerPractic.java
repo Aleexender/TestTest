7f2f8896c4d5c10704f4dc70a6c49441999dc02a
Aleexender <alexkjh2011@gmail.com>
2024-07-19 18:12:55 +0900

    Update IntegerPractic.java

--- src/practice/IntegerPractic.java
+++ src/practice/IntegerPractic.java
@@ -1,2 +1,31 @@
-package practice;public class IntegerPractic {
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntegerPractic {
+    public static void main(String[] args) {
+        int[] a = new int[5]; // 사이즈 값을 설정 해줘야한다.
+        int[] b = {1, 2, 3, 4,5,3,2,1,};
+
+
+        LamdaTe lamdaTe = new LamdaTe();
+
+        List<Integer> array = new ArrayList<>(); // 무한대 -> 메모리
+        int[][] arr = new int[5][5];
+
+        int[] c = new int[4];
+
+
+        c[0] = 4;
+        c[4] = 5;
+        boolean[] b1 = new boolean[4]; //flag
+        Arrang3 arrang3 = new Arrang3();
+
+
+        String[] b2 = new String[6];
+        a[0] = 5;
+        a[1] = 3;
+        a[2] = 4;
+    }
 }
