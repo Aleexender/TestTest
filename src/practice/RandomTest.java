e6534750630d2ad3e2988d84b9671a0ddddf9608
Aleexender <alexkjh2011@gmail.com>
2024-07-30 00:45:32 +0900

    Update RandomTest.java

--- src/practice/RandomTest.java
+++ src/practice/RandomTest.java
@@ -1,2 +1,15 @@
-package practice;public class RandomTest {
+package practice;
+
+import java.util.Random;
+
+public class RandomTest {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        for (int i = 0; i < 5; i++) {
+            int a = random.nextInt(45);
+            System.out.println(a);
+        }
+
+    }
 }
