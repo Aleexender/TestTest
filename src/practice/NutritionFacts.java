0b7090526c699a737e2c2307a6b2190a926e7d86
Aleexender <alexkjh2011@gmail.com>
2024-08-02 23:41:00 +0900

    Create NutritionFacts.java

---
+++ src/practice/NutritionFacts.java
@@ -0,0 +1,72 @@
+package practice;
+
+public class NutritionFacts {
+
+    private final int serving;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    private NutritionFacts(Builder builder) {
+        serving = builder.serving;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+
+    public static class Builder {
+
+        // 필수
+        private final int serving;
+        private final int servings;
+
+        // 선택
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int serving, int servings) {
+            this.serving = serving;
+            this.servings = servings;
+        }
+
+        public Builder calories(int calories) {
+            this.calories = calories;
+            return this;
+        }
+
+        public Builder fat(int fat) {
+            this.fat = fat;
+            return this;
+        }
+
+        public Builder sodium(int sodium) {
+            this.sodium = sodium;
+            return this;
+        }
+
+        public Builder carbohydrate(int carbohydrate) {
+            this.carbohydrate = carbohydrate;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+                .calories(100)
+                .sodium(35)
+                .carbohydrate(27)
+                .build();
+    }
+}
+
+// 사용 예시
