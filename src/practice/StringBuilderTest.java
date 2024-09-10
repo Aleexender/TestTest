46577f3f4e5eb8042108775609ed631a26b13300
Aleexender <alexkjh2011@gmail.com>
2024-09-10 19:02:49 +0900

    Update StringBuilderTest.java

--- src/practice/StringBuilderTest.java
+++ src/practice/StringBuilderTest.java
@@ -1,2 +1,31 @@
-package practice;public class StringBuilderTest {
+package practice;
+
+import java.util.LinkedList;
+
+public class StringBuilderTest {
+    public static void main(String[] args) {
+        String str = "ABC";
+        String str2 = "DEFG";
+        String strResult = str + str2;
+
+        System.out.println(strResult);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("123").append("ABC");
+
+        stringBuilder.insert(2, "CCC");
+        stringBuilder.delete(3, 5);
+
+        System.out.println(stringBuilder.toString());
+
+        StringBuffer buffer = new StringBuffer();
+
+        LinkedList<Integer> qwe = new LinkedList<>();
+
+
+        String[] qwew = new String[10];
+
+        int length = qwew.length;
+    }
 }
